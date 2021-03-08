395635d5c3df96c1b13e33849d94db7415181c6b
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-03-08 13:29:39 +0100

    changed server port to 3030

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.route('/').get((req, res) => {
-    res.sendFile(__dirname+'/index.html');
-})
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
-mongoose.connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+app.route("/").get((req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('server is running on port: 5000');
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(process.env.PORT || 3030, () => {
+  console.log("server is running on port: 3030");
 });
